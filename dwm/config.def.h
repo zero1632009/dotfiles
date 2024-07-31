7a93601dd2b9822028659819537827f5a2b092ca
Your Name <you@example.com>
2024-07-31 16:52:08 +0700

    up

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -55,7 +55,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL};
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */

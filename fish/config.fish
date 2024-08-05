20c6688542848f8fc8935c34d2c3455e4b1272b1
John Doe <johndoe@example.com>
2024-08-05 23:14:56 +0700

    upload files

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+##### Fish Config #####
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Remove Welcome
+set fish_greeting
+
+# Env
+alias ls="exa -l"
+alias la="exa -la"
+alias open="xdg-open"
+
+fish_config theme choose "agnoster"
